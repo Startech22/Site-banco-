0acd0f0c20bc8ce6f23638dcb5d2648cf191cf68
Ale <108987903+Startech22@users.noreply.github.com>
2022-11-30 19:12:49 -0300

    Add files via upload

---
+++ site banco/js/banco.js
@@ -0,0 +1,95 @@
+function ValidaSenha(nome, senha) {
+    let Cadastrado = [];
+    let Senha = [];
+    for (var i = 0; i < usuarios.length; i++) {
+    
+      if (usuarios[i].nome === nome) {
+        Cadastrado = "Usuario OK"
+        if (usuarios[i].senha === senha) {
+          Senha = "Senha OK"
+        }
+      }
+    }
+}
+
+const usuarios = [
+    {nome: "Alessandra", senha: "101075"},
+  ];
+
+  
+let inputs = document.getElementsByClassName('input-form');
+for (let input of inputs) {
+    input.addEventListener("blur", function() {
+    if(input.value.trim() != "") {
+      input.classList.add("has-val");
+    } else {
+      input.classList.remove("has-val");
+      }
+    });
+  }
+  /*   banner
+
+  // Seleção de elementos
+const menuBtn = document.querySelector("#menu");
+const closeMenuBtn = document.querySelector("#close-menu");
+const menu = document.querySelector("#mobile-navbar");
+
+const desktopLinks = document.querySelectorAll("#navbar a");
+const mobileLinks = document.querySelectorAll("#mobile-navbar a");
+const allLinks = [...desktopLinks, ...mobileLinks];
+
+const slides = document.querySelectorAll(".banner");
+const dots = document.querySelectorAll(".dot");
+let slideIndex = 0;
+
+// Funções
+function smoothScroll(e) {
+  e.preventDefault();
+
+  const href = this.getAttribute("href");
+  const offsetTop = document.querySelector(href).offsetTop;
+
+  scroll({
+    top: offsetTop,
+    behavior: "smooth",
+  });
+
+  setTimeout(() => {
+    if (menu.classList.contains("menu-active")) {
+      menu.classList.remove("menu-active");
+    }
+  }, 500);
+}
+
+function showSlides() {
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].classList.remove("active");
+    dots[i].classList.remove("active");
+  }
+
+  slideIndex++;
+
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+
+  slides[slideIndex - 1].classList.add("active");
+  dots[slideIndex - 1].classList.add("active");
+
+  setTimeout(showSlides, 3000);
+}
+
+// Eventos
+[menuBtn, closeMenuBtn].forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    menu.classList.toggle("menu-active");
+  });
+});
+
+allLinks.forEach((link) => {
+  link.addEventListener("click", smoothScroll);
+});
+
+// Inicialização
+showSlides();
+*/
